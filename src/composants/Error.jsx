ce51edd460ed986363c47427ac67260cee1da2c4
margaux <margaux.tarayre@gmail.com>
2023-08-16 16:24:40 +0200

    Adding static text with style in the 404 error page

---
+++ src/composants/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Error() {
+    return (
+        <div className="error-message">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+        </div>
+    )
+}
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Accordion from '../composants/accordion';
 
-export default function Logement() {
+export default function Logement() 
+{
   const {logementId}=useParams();
+  const images = [
+    '../images/banner-about.png',
+    '../images/banner-home.png',
+    //'/path/to/image3.jpg',
+    // ... more image paths
+  ];
+  const [activeIndex, setActiveIndex] = useState(0); // By default, the first image is active
   return (
-    <div>Logement {logementId}</div>
+    <div>
+      Logement {logementId}
+      {/*  */}
+      {activeIndex > 0 && <button onClick={() => setActiveIndex(activeIndex - 1)}>←</button>}
+            
+            {images.map((src, index) => (
+                <Accordion key={index} imageSrc={src} open={index === activeIndex} />
+            ))}
+
+            {activeIndex < images.length - 1 && <button onClick={() => setActiveIndex(activeIndex + 1)}>→</button>}
+    </div>
+
   )
 }
